55ca0f75789f9ba5a917849506d6940919d8a9c5
PedroOrtega88 <140651151+PedroOrtega88@users.noreply.github.com>
2023-11-10 00:21:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
  // Aquí tu código
 
- const lista = document.getElementById('lista');  // Corregir paréntesis aquí
+ const lista = document.getElementById('lista'); 
  const agregar = document.getElementById('agregar');
  
  agregar.addEventListener('click', function(){
      let cartel = prompt('Agregar elemento');
-     if (cartel !== null) {  // Verificar si el usuario hizo clic en "Cancelar"
+     if (cartel !== null) {  
          let cartelOne = document.createElement('li');
          cartelOne.innerText = cartel;
          lista.appendChild(cartelOne);
